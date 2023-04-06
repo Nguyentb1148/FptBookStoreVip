e0282ce7c678e143c2f7a3998469f35624c25283
Rimsuano1 <huynhngochoangloc2002@gmail.com>
2023-04-06 09:46:21 +0700

    Create Private ApplicationDbContex

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FptBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FptBookStore.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+
+    }
     // GET
     public IActionResult Index()
     {
+        var objCategoryList = _db.Category.ToList();
         return View();
     }
 }
